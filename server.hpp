2790a38ccb40e4c0dd56b7679a917c9e5fb4e23b
Suguru Hamazaki <hamazy@webspiritus.com>
2011-05-20 05:50:22 +0900

    deliver line-based messages

--- server.hpp
+++ server.hpp
@@ -5,6 +5,7 @@
 #include <set>
 #include <algorithm>
 #include <queue>
+#include <string>
 
 #include <boost/thread.hpp>
 #include <boost/asio.hpp>
@@ -14,32 +15,11 @@
 
 namespace shiritori {
 
-class message
-{
-	enum { message_length = 8 };
-	char data_[message_length];
-public:
-	message() {}
-	virtual ~message() {}
-	char *data()
-	{
-		return data_;
-	}
-	char const *data() const
-	{
-		return data_;
-	}
-	std::size_t size() const
-	{
-		return message_length;
-	}
-};
-
 class player {
 public:
 	typedef boost::shared_ptr<player> pointer;
 	virtual ~player() {}
-	virtual void deliver(message const &message) = 0;
+	virtual void deliver(std::string const &message) = 0;
 };
 
 class game
@@ -60,7 +40,7 @@ public:
 	{
 		players_.erase(player);
 	}
-	void deliver(message const &message)
+	void deliver(std::string const &message)
 	{
 		std::for_each(
 			players_.begin(), players_.end(),
@@ -79,18 +59,11 @@ class session
 {
 	boost::asio::ip::tcp::socket socket_;
 	game &game_;
-	message message_;
-	std::queue<message> message_queue_;
+	boost::asio::streambuf buffer_;
+	std::queue<std::string> message_queue_;
 
 public:
 	typedef boost::shared_ptr<session> pointer;
-
-	session(boost::asio::io_service& io_service, game &game)
-		: socket_(io_service)
-		, game_(game)
-		, message_()
-		, message_queue_()
-	{}
 
 	virtual ~session() {}
 
@@ -102,18 +75,23 @@ public:
 		read_message();
 	}
 
-	void handle_read(boost::system::error_code const &error)
+	void on_read(boost::system::error_code const &error, std::size_t bytes_transferred)
 	{
 		if (error)
 		{
 			leave_from_game();
 			return;
 		}
-		game_.deliver(message_);
+
+		std::string line;
+		std::istream stream(&buffer_);
+		std::getline(stream, line);
+		
+		game_.deliver(line + "\r\n");
 		read_message();
 	}
 
-	void handle_write(boost::system::error_code const &error)
+	void on_write(boost::system::error_code const &error)
 	{
 		if (error)
 		{
@@ -126,7 +104,7 @@ public:
 		write_front_message();
 	}
 
-	void deliver(message const &message_to_deliver)
+	void deliver(std::string const &message_to_deliver)
 	{
 		const bool write_in_progress(!message_queue_.empty());
 		message_queue_.push(message_to_deliver);
@@ -140,23 +118,30 @@ public:
 	}
 
 private:
+	session(boost::asio::io_service& io_service, game &game)
+		: socket_(io_service)
+		, game_(game)
+		, buffer_()
+		, message_queue_()
+	{}
+
 	void read_message()
 	{
-		boost::asio::async_read(
-			socket_,
-			boost::asio::buffer(message_.data(), message_.size()),
+		boost::asio::async_read_until(
+			socket_, buffer_, "\r\n",
 			boost::bind(
-				&session::handle_read, shared_from_this(),
-				boost::asio::placeholders::error));
+				&session::on_read, shared_from_this(),
+				boost::asio::placeholders::error,
+				boost::asio::placeholders::bytes_transferred));
 	}
 	void write_front_message()
 	{
-		const message message_delivering(message_queue_.front());
+		std::string const message_delivering(message_queue_.front());
 		boost::asio::async_write(
 			socket_,
-			boost::asio::buffer(message_delivering.data(), message_delivering.size()),
+			boost::asio::buffer(message_delivering),
 			boost::bind(
-				&session::handle_write, shared_from_this(),
+				&session::on_write, shared_from_this(),
 				boost::asio::placeholders::error));
 	}
 	void leave_from_game()
